1b464ae436bc02ba1d91e40de1fd7cc2ac96cd36
Arpit Kumar <arpitkumar4000@gmail.com>
2024-01-16 20:58:17 +0530

    Edited cors

--- src/main/java/com/restapi/newsapp/configuration/CorsConfiguration.java
+++ src/main/java/com/restapi/newsapp/configuration/CorsConfiguration.java
@@ -1,10 +1,15 @@
 package com.restapi.newsapp.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Value("${cors.origin}")
+    private String origin;
+
     @Override
     public void addCorsMappings(
             org.springframework.web.servlet.config.annotation.CorsRegistry registry
@@ -12,7 +17,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
         registry
                 .addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(origin)
                 .allowedHeaders("*");
     }
 }
